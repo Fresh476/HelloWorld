a3b686d5a47aa0a4640999b3fd776e3dd8e19941
司濯桐 <2523441314@qq.com>
2026-03-31 22:12:24 +0800

    初步修改了main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include <event2/event.h>
 
 int main()
 {
-	const char** p = NULL;
-	//p = event_get_supported_methods();
-	int i = 0;
-	while (p[i])
-	{
-		printf("%s\n", p[i++]);
-	}
-	printf("\n");
-	//printf("OK\n");
+	printf("This is a HelloWorld!\n");
 	return 0;
 }
