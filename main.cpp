212d60abd2561aa23477e17815bb5c40c0453389
司濯桐 <2523441314@qq.com>
2026-04-01 21:45:32 +0800

    add win_branch()

--- main.cpp
+++ main.cpp
@@ -6,11 +6,16 @@ void fun(){
 	printf("nihao\n");
 }
 
+void win_branch(){
+	printf("This is win_branch.\n");
+}
+
 int main()
 {
 	printf("This is a HelloWorld!\n");
 	fun();
 	printf("ubuntu.\n");
 	printf("new windows.\n");
+	win_branch();
 	return 0;
 }
